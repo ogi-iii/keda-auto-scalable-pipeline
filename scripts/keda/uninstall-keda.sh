16d2037be11a20649aa159803166d154e73c9f87
ogi-iii <kazuki.aape.09@gmail.com>
2023-04-22 22:50:13 +0900

    commands updated

--- scripts/keda/uninstall-keda.sh
+++ scripts/keda/uninstall-keda.sh
@@ -2,6 +2,6 @@
 helm uninstall keda -n keda
 kubectl delete namespace keda
 
-docker image rmi -f \$(docker image ls ghcr.io/kedacore/keda-admission-webhooks --format "{{.ID}}")
-docker image rmi -f \$(docker image ls ghcr.io/kedacore/keda-metrics-apiserver --format "{{.ID}}")
-docker image rmi -f \$(docker image ls ghcr.io/kedacore/keda --format "{{.ID}}")
+docker image rmi -f $(docker image ls ghcr.io/kedacore/keda-admission-webhooks --format "{{.ID}}")
+docker image rmi -f $(docker image ls ghcr.io/kedacore/keda-metrics-apiserver --format "{{.ID}}")
+docker image rmi -f $(docker image ls ghcr.io/kedacore/keda --format "{{.ID}}")
